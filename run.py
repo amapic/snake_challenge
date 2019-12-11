abc6dc9d5cc555b0adffe7342c18abb54e751224
amapic <amaury.pichat@gmail.com>
2019-12-11 13:58:04 +0000

    Update run.py

--- run.py
+++ run.py
@@ -80,7 +80,7 @@ def run():
 		print(folder)
 		
     classes = get_snake_classes()
-
+    print(h)
     def softmax(x):
         """Compute softmax values for each sets of scores in x."""
         e_x = np.exp(x - np.max(x))
